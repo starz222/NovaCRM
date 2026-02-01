155fe47e94f2827f00426abb3b888d5e4f10c459
starz222 <steve@stationbreak.com>
2026-02-01 11:29:55 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // This part bridges the gap between Vercel settings and your code
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
     'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY),
   },
   build: {
     outDir: 'dist',
+    sourcemap: false
   }
 });
